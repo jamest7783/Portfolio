f68ea7ce36299614fcbf131bb35f13b9ad50c78b
James Jewitt <jamestjewitt@gmail.com>
2022-08-30 13:53:03 -0400

    Reconfigure text links

--- client/src/components/LinkLayer.jsx
+++ client/src/components/LinkLayer.jsx
@@ -5,16 +5,28 @@ const LinkLayer=()=>{
     return(
         <div id='link-layer'>
             <div className='link-level'>
-                <button><div className='icon' id='mail-icon'></div>jamestjewitt@gmail.com</button>
-                <button>resume<div className='icon' id='resume-icon'></div></button>
+                <button>
+                    <div className='icon' id='mail-icon'></div>email
+                </button>
+                <button>
+                    resume<div className='icon' id='resume-icon'></div>
+                </button>
             </div>
             <div className='link-level'>
-                <button><div className='icon' id='github-icon'></div>gitHub</button>
-                <button>skills<div className='icon' id='code-icon'></div></button>
+                <button>
+                    <div className='icon' id='github-icon'></div>gitHub
+                </button>
+                <button>
+                    skills<div className='icon' id='code-icon'></div>
+                </button>
             </div>
             <div className='link-level'>
-                <button><div className='icon' id='trello-icon'></div>trello boards</button>
-                <button>projects<div className='icon' id='hammer-icon'></div></button>
+                <button>
+                    <div className='icon' id='trello-icon'></div>trello
+                </button>
+                <button>
+                    projects<div className='icon' id='hammer-icon'></div>
+                </button>
             </div>
         </div>
     )

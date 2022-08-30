1f848835facb2df9b68767bafcf1b4c63c6d6d6a
James Jewitt <jamestjewitt@gmail.com>
2022-08-30 14:30:01 -0400

    Render hidden view on Globe

--- client/src/components/LinkLayer.jsx
+++ client/src/components/LinkLayer.jsx
@@ -1,33 +1,42 @@
-
+import {useNavigate} from "react-router-dom"
 
 const LinkLayer=()=>{
 
+    const link=(path)=>{window.open(path,'_blank')}
+    
     return(
         <div id='link-layer'>
             <div className='link-level'>
-                <button>
-                    <div className='icon' id='mail-icon'></div>email
+                <button onClick={(e)=>{link('https://google.com')}}>
+                    <div className='icon' id='mail-icon'></div>
+                    contact
                 </button>
-                <button>
-                    resume<div className='icon' id='resume-icon'></div>
-                </button>
-            </div>
-            <div className='link-level'>
-                <button>
-                    <div className='icon' id='github-icon'></div>gitHub
-                </button>
-                <button>
-                    skills<div className='icon' id='code-icon'></div>
+                <button onClick={(e)=>{link('https://docs.google.com/document/d/1jYzRgLNj8IO4ZzwQ2WoLnseSsjhxjxDhzHmBwpee2Mc/edit?usp=sharing')}}>
+                    resume
+                    <div className='icon' id='resume-icon'></div>
                 </button>
             </div>
             <div className='link-level'>
-                <button>
-                    <div className='icon' id='trello-icon'></div>trello
+                <button onClick={(e)=>{link('https://github.com/jamest7783')}}>
+                    <div className='icon' id='github-icon'></div>
+                    gitHub
                 </button>
-                <button>
-                    projects<div className='icon' id='hammer-icon'></div>
+                <button onClick={(e)=>{link('https://google.com')}}>
+                    skills
+                    <div className='icon' id='code-icon'></div>
                 </button>
             </div>
+            <div className='link-level'>
+                <button onClick={(e)=>{link('https://www.linkedin.com/in/james-jewitt/')}}>
+                    <div className='icon' id='linkedin-icon'></div>
+                    linkedin
+                </button>
+                <button onClick={(e)=>{link('https://google.com')}}>
+                    projects
+                    <div className='icon' id='hammer-icon'></div>
+                </button>
+            </div>
+            <div id='hidden-view'></div>
         </div>
     )
 }

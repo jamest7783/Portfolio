b419976bb21be1a8034694381346291bab2be79c
James Jewitt <jamestjewitt@gmail.com>
2022-08-31 22:21:51 -0400

    Working on setting style index

--- client/src/components/LinkLayer.jsx
+++ client/src/components/LinkLayer.jsx
@@ -7,30 +7,39 @@ const LinkLayer=({styles,styleIndex,setStyleIndex})=>{
         console.log('here')
         setStyleIndex(num)
     }
+    console.log(styles)
+    let outline_color=styles[styleIndex].outline_color
+    console.log(outline_color)
+
+
+
 
     return(
-        <div id='link-layer'>
-            <button className='moon' id='_0' onClick={(e)=>{styleize(1)}}>
+        <div id='link-layer' style={{ outlineColor:outline_color }}>
+            <button className='moon' id='_0' onClick={(e)=>{styleize(6)}}>
                 <div className='icon' id='mail-icon'></div>
                 contact
             </button>
-            <button className='moon' id='_1' onClick={(e)=>{styleize(2)}}>
+            <button className='moon' id='_1' onClick={(e)=>{styleize(1)}}>
                 <div className='icon' id='resume-icon'></div>
                 resume
             </button>
-            <button className='moon' id='_2'>
+            <button className='moon' id='_2' onClick={(e)=>{styleize(2)}}>
                 <div className='icon' id='github-icon'></div>
                 github
             </button>
-            <button className='moon' id='_3'>
+            <button className='moon' id='_3' onClick={(e)=>{styleize(3)}}>
                 <div className='icon' id='code-icon'></div>
                 skills
             </button>
-             <button className='moon' id='_4' onClick={(e)=>{link('https://www.linkedin.com/in/james-jewitt/')}}>
+             <button className='moon' id='_4' onClick={(e)=>{
+                link('https://www.linkedin.com/in/james-jewitt/')
+                styleize(4)
+                }}>
                 <div className='icon' id='linkedin-icon'></div>
                 linkedin
             </button>
-            <button className='moon' id='_5' onClick={(e)=>{link('https://google.com')}}>
+            <button className='moon' id='_5' onClick={(e)=>{styleize(5)}}>
                 <div className='icon' id='hammer-icon'></div>
                 projects
             </button>
